52f910e06af1f38bd8827260ec5e133e6f0fc731
hdz-kevin <kevinhdez592@gmail.com>
2025-06-02 10:21:10 -0600

    add breeze and customize views

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,34 @@
+<x-guest-layout>
+  <div class="mb-4 text-sm text-gray-600">
+    {{-- {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }} --}}
+    {{ __('Forget your password? Enter your registration email for and we will send you a link so you can choose a new one') }}
+  </div>
+
+  <!-- Session Status -->
+  <x-auth-session-status class="mb-4" :status="session('status')" />
+
+  <form method="POST" action="{{ route('password.email') }}">
+    @csrf
+
+    <!-- Email Address -->
+    <div>
+      <x-input-label for="email" :value="__('Email')" />
+      <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+        autofocus />
+      <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    </div>
+
+
+    <div class="flex items-center justify-between mt-4">
+      <x-form-link :href="route('login')">
+        Already registered?
+      </x-form-link>
+      <x-form-link :href="route('register')">
+        Create Account
+      </x-form-link>
+    </div>
+    <x-primary-button class="w-full justify-center mt-4">
+      {{ __('Email Password Reset Link') }}
+    </x-primary-button>
+  </form>
+</x-guest-layout>
---
+++ resources/views/components/form-link.blade.php
@@ -0,0 +1,4 @@
+<a
+  {{ $attributes->merge(['class' => 'text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none']) }}>
+  {{ $slot }}
+</a>
