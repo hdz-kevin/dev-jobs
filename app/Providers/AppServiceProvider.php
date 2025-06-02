afb57fd584b7b36af599a8fae496fdfe2ffffe30
hdz-kevin <kevinhdez592@gmail.com>
2025-06-02 10:22:20 -0600

    abilitate email verification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        # Customize verification email
+        // VerifyEmail::toMailUsing(
+        //     fn ($notifiable, $url) => (new MailMessage)
+        //         ->subject('Varificar Cuenta')
+        //         ->line('Tu cuenta ya está casi lista, solo debes presionar el enlace a continuación')
+        //         ->action('Confirmar Cuenta', $url)
+        //         ->line('Si no creaste esta cuenta, puedes simplemente ignorar este mensaje')
+        // );
     }
 }
