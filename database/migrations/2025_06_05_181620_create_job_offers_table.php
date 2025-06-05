09fc76b6e8d001d8d6b2971fd090d0f9542d4735
hdz-kevin <kevinhdez592@gmail.com>
2025-06-05 12:43:21 -0600

    recreate migration for job_offers table

--- database/migrations/2025_06_05_181620_create_job_offers_table.php
+++ database/migrations/2025_06_05_181620_create_job_offers_table.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Category;
+use App\Models\Salary;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +16,15 @@ return new class extends Migration
     {
         Schema::create('job_offers', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('company');
+            $table->date('due_date');
+            $table->text('description');
+            $table->string('image');
+            $table->tinyInteger('published')->default(1);
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(Salary::class)->constrained();
             $table->timestamps();
         });
     }
